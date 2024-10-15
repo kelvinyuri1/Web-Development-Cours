7321d6def4fe27140d9c6f7b646afcf4b07aad27
Kelvin Yuri Silva Felix <kelvin.yuri012@gmail.com>
2024-10-15 20:22:51 -0300

    add class 02 operators

---
+++ 2-JavaScript/Class02-Operators/script.js
@@ -0,0 +1,58 @@
+// operadores aritimeticos
+
+let num1 = 2
+let num2 = 3
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(81 ** (1 / 2));
+
+// operadores de atribuição (assigment)
+
+  num1 = 10
+  num2 = num2 + 2
+  num1 = num1 - 6
+
+  num1 += 4
+  num2 -= 2
+
+  console.log(num1);
+  console.log(num2);
+  
+//   operadores de comparação (comparison)
+
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(2 < 1);
+console.log(2 > 1);
+console.log(2 >= 2);
+console.log(20 === 20);
+
+// logica (logical, && ||)
+
+const credit = true
+const card = true
+// Disney
+console.log(credit && card);
+
+// E (&&)
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+// ou (||)
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+
+
+
+
+
+
+  
