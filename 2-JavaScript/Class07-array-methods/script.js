ecdcf3518336ebe7b5639f661ec3e39811f922e9
Kelvin Yuri Silva Felix <kelvin.yuri012@gmail.com>
2024-11-06 20:28:13 -0300

    add class 07 array methods

--- 2-JavaScript/Class07-array-methods/script.js
+++ 2-JavaScript/Class07-array-methods/script.js
@@ -1,6 +1,6 @@
-// MAP
+// MAP (immutable)🌟
 
-const numbers = [10, 20, 30]
+const numbers = [10, 20, 30];
 console.log(numbers.map((num) => num + 1));
 console.log(numbers.map((num) => num * 5.74));
 
@@ -17,7 +17,47 @@ console.log(
 );
 
 const newSalaries = devs.map((dev) => {
-    return parseFloat(dev.salary * 1.1).toFixed(0);
-  })
+  return parseFloat(dev.salary * 1.1).toFixed(0);
+});
 console.log(newSalaries);
 
+//filter (immutable)🌟
+
+// const devFiltered = devs.filter((dev) => dev.salary > 6000);
+// console.log(devFiltered);
+
+// const resp = prompt("Digite o nome: ").toLocaleLowerCase();
+
+// const resultFilter = devs.filter((dev) =>
+//   dev.name.toLocaleLowerCase().includes(resp)
+// );
+// console.log(resultFilter);
+
+// sort
+
+const numbers2 = [1, 4, 9, "a", "A", 2, 10, 33, "Kelvin"];
+console.log(numbers2);
+numbers2.sort();
+numbers2.sort((a, b) => a - b); // Organiza os numericos
+
+console.log(numbers2);
+
+// Para organizar tudo tanto caractere quanto numero por ordem alfabetica
+numbers2.sort((a, b) => {
+  if (typeof a == "string" && typeof b == "string") {
+    a = String(a).toLocaleLowerCase();
+    b = String(b).toLocaleLowerCase();
+  }
+
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
+});
+
+console.log(numbers2);
+
+// reduce
+
+const expenses = [100,20, 32, 38, 220, 50];
+const total = expenses.reduce((acc , b) => acc + b)
+console.log(total);
